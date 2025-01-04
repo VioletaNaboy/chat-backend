d09a177dd13b7e629865f457b51f4723e994cd9c
VioletaNaboy <121202853+VioletaNaboy@users.noreply.github.com>
2025-01-04 22:04:54 +0100

    fixed

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,11 +1,15 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { User } from '../models/User';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID as string,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    callbackURL: "http://https://chat-backend-ofrx.onrender.com/auth/google/callback"
+    callbackURL: "https://chat-backend-ofrx.onrender.com/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
